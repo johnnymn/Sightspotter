49aa6c2b2c3f576e4eb68816713ff806e433a874
Johnny Martinez <johnnymn@hotmail.com>
2021-05-04 12:31:07 -0700

    Fix API URL multiline string

--- Sightspotter/ViewController.swift
+++ Sightspotter/ViewController.swift
@@ -129,12 +129,12 @@ class ViewController: UIViewController,
   /// observing the user's heading.
   func fetchSights() {
     let urlString = """
-                    https://en.wikipedia.org/w/api.php?
-                    ggscoord=\(userLocation.coordinate.latitude)
-                    %7C\(userLocation.coordinate.longitude)
-                    &action=query&prop=coordinates%7Cpageimages
-                    %7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500
-                    &pilimit=50&wbptterms=description&generator=geosearch
+                    https://en.wikipedia.org/w/api.php?\
+                    ggscoord=\(userLocation.coordinate.latitude)\
+                    %7C\(userLocation.coordinate.longitude)\
+                    &action=query&prop=coordinates%7Cpageimages\
+                    %7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500\
+                    &pilimit=50&wbptterms=description&generator=geosearch\
                     &ggsradius=10000&ggslimit=50&format=json
                     """
     guard let url = URL(string: urlString) else {
