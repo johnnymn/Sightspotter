ae432a780f4b35f5ee7240e4024b8dbb6644f2c3
Johnny Martinez <johnnymn@hotmail.com>
2021-05-04 08:55:44 -0700

    Add the boilerplate for handling the JSON data from Wikipedia's API

--- Sightspotter/ViewController.swift
+++ Sightspotter/ViewController.swift
@@ -2,11 +2,21 @@ import ARKit
 import CoreLocation
 import UIKit
 import SpriteKit
+import SwiftyJSON
 
 class ViewController: UIViewController,
         ARSKViewDelegate, CLLocationManagerDelegate {
   let locationManager = CLLocationManager()
   var userLocation = CLLocation()
+
+  var userHeading = 0.0
+  // Keep track of how many heading
+  // updates we receive from the locationManager.
+  var headingCount = 0
+
+  // Store the JSON object we get
+  // from the Wikipedia's API.
+  var sightsJSON: JSON!
 
   @IBOutlet var sceneView: ARSKView!
 
@@ -86,7 +96,28 @@ class ViewController: UIViewController,
     }
   }
 
+  /// Fetch the sights from the Wikipedia API,
+  /// store them as a JSON string, and start
+  /// observing the user's heading.
   func fetchSights() {
+    let urlString = """
+                    https://en.wikipedia.org/w/api.php?
+                    ggscoord=\(userLocation.coordinate.latitude)
+                    %7C\(userLocation.coordinate.longitude)
+                    &action=query&prop=coordinates%7Cpageimages
+                    %7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500
+                    &pilimit=50&wbptterms=description&generator=geosearch
+                    &ggsradius=10000&ggslimit=50&format=json
+                    """
+    guard let url = URL(string: urlString) else {
+      return
+    }
 
+    // Data(contentsOf: URL) throws
+    // so we wrap it with a try?
+    if let data = try? Data(contentsOf: url) {
+      sightsJSON = JSON(data)
+      locationManager.startUpdatingHeading()
+    }
   }
 }
